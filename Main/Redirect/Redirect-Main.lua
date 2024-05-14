5ae58148e194cc076abf65ab2f882c6f199bf6ee
SourcexCode <robinmaterr@gmail.com>
2024-05-14 12:30:15 +0200

    Update Redirect-Main.lua

--- Main/Redirect/Redirect-Main.lua
+++ Main/Redirect/Redirect-Main.lua
@@ -1,4 +1,4 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/F4ir/UI-Librarys/main/Script/Mod/cmds.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/F4ir/UI-Librarys/main/Script/Anti-Mod/Anti-Mod.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/F4ir/UI-Librarys/main/Script/Script/D3V1LS-Source.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/F4ir/UI-Librarys/main/Script/Emoijs/Owner.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SourcexCode/D3V1LS-Source/main/Script/AddOns/Premium/Premium-Commands%26Whitelist.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SourcexCode/D3V1LS-Source/main/Script/Anti-Mod/Anti-Mod.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SourcexCode/D3V1LS-Source/main/Script/Script/D3V1LS-Source.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SourcexCode/D3V1LS-Source/main/Script/Emoijs/Emoijs-Script-Redirect.lua"))()
