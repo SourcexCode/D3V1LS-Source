678b61ba3d7869723b4476bf966585e8426e3a18
SourcexCode <robinmaterr@gmail.com>
2024-05-14 18:29:03 +0200

    Update D3V1LS-Source.lua

--- Script/Script/D3V1LS-Source.lua
+++ Script/Script/D3V1LS-Source.lua
@@ -168,7 +168,7 @@ local Tab6 = Window:Tab("Premium",false)
 Tab6:Label("Discord In Credits")
 
 Tab6:Button("Premium GUI", function(bool)
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/SourcexCode/D3V1LS-Source/main/Script/AddOns/Premium/Whitelist.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/SourcexCode/D3V1LS-Source/main/Script/Premium/Whitelist.lua"))()
 end)
 
 
