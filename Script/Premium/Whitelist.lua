ad4b692881f6320687061de1153b8e8923b0b60b
SourcexCode <robinmaterr@gmail.com>
2024-05-15 21:23:39 +0200

    Update Whitelist.lua

--- Script/Premium/Whitelist.lua
+++ Script/Premium/Whitelist.lua
@@ -7,6 +7,7 @@ local scriptWhitelist = {
     [3647872448] = true,   -- pendalove3
     [3710265398] = true,   -- snorlax01047
     [5421324166] = true,   -- chiefkeefdedicated
+    
 }
 
 local function executeScript()
