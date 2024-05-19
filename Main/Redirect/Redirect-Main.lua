77848bdb29280a703d0d24e8662f81908cd5d9b2
SourcexCode <robinmaterr@gmail.com>
2024-05-20 01:29:17 +0200

    Update Redirect-Main.lua

--- Main/Redirect/Redirect-Main.lua
+++ Main/Redirect/Redirect-Main.lua
@@ -1,4 +1,5 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/SourcexCode/D3V1LS-Source/main/Script/Premium/Premium-Commands%26Whitelist.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/SourcexCode/D3V1LS-Source/main/Script/Anti-Mod/Anti-Mod.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/SourcexCode/D3V1LS-Source/main/Script/Script/D3V1LS-Source.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SourcexCode/D3V1LS-Source/main/Script/Sofie/Commands/OwnerTeleporter.txt"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/SourcexCode/D3V1LS-Source/main/Script/Emoijs/Emoijs-Script-Redirect.lua"))()
